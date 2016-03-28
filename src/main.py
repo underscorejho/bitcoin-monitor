dd763c59f9fca97768132b8eeb4b7b9dc09ae904
Jared Henry Oviatt <jaredhenryoviatt@gmail.com>
2016-03-28 15:34:25 +1300

    read/write ledger

--- src/main.py
+++ src/main.py
@@ -10,10 +10,10 @@
 # write ledger to file on run
 # use ledger to keep track of buys/sells instead of db
 # read day and balance from ledger
-# ... 'Day: 5, Bought .xx BTC for $xx.xx' OR 'Day: 5, Sold x.xx BTC for $xxx.xx at xxx.xx% profit
+# ... 'Day: 5, Bought $xx.xx' OR 'Day: 5, Sold $xxx.xx at xxx.xx% profit
 # ... 'Day: 5, Balance: $5000, Buys: 3, BTC: .23, Investment: $105'
 # set cron job to run daily
-# run on every 5th/10th day
+# run on every 5th/10th day (two cron jobs)
 # deposit 'deposit' every 30th day
 # set up email notification reporting results (python main > mailto...)
 # ... action - (buy $xxx.xx today OR (if past nbuys) sell at $xxx.xx for xxx% profit)
@@ -25,6 +25,43 @@
 from urllib.request import urlopen
 import sys
 
+def get_ledger():
+  f = open('ledger.ldg', 'r')
+  ledger = f.readlines()
+  f.close
+  
+  state = ledger[-1].split(',')
+  state = [x.strip() for x in state]
+  
+  return int(state[0][5:]), float(state[1][10:]), int(state[2][6:]), float(state[3][5:]), float(state[4][13:])
+
+def write_ledger(ledger_list):
+  # ledger_list = [day+1, balance, buys, btc, cost, action, amount]
+  buy_str = 'Day: ' + str(ledger_list[0]) + ',Bought $' + str(ledger_list[6]) 
+  sell_str = 'Day: ' + str(ledger_list[0]) + ', Sold $' + str(ledger_list[6]) + 'for ' + str(ledger_list[4]/ledger_list[6]) + '% profit'
+  summmary_str = 'Day: ' + str(ledger_list[0]) + ', Balance: $' + str(ledger_list[1]) + ', Buys: ' + str(ledger_list[2]) + ', BTC: ' + str(ledger_list[3]) + ', Investment: $' + str(ledger_list[4])
+
+  if ledger_list[5] == 'buy':
+    action_str = buy_str
+  elif ledger_list[5] == 'sell':
+    action_str = sell_str
+  else:
+    print('ERROR: wut')
+
+  f = open('ledger.ldg', 'r')
+  ledger = f.readlines()
+  f.close()
+
+  ledger = ledger[:-1]
+  ledger.append(action_str)
+  ledger.append(summary_str)
+
+  f = open('ledger.ldg', 'w')
+  f.writelines(ledger)
+  f.close
+
+  
+  return
 
 def strategy(skip, nbuys):
   
@@ -96,7 +133,8 @@ def strategy(skip, nbuys):
     print("Cost : " + str(cost) + "; Day : " + str(day))
 ###
   
-  write_ledger(day+1, balance, buys, btc, cost)
+  ledger_list = [day+1, balance, buys, btc, cost, action, amount]
+  write_ledger(ledger_list)
 
   return balance
 
